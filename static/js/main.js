e884bbb54a0786027c3bb93261006f18b3d9df22
Tuan Nguyen <tuanzw@gmail.com>
2022-08-25 14:57:22 +0700

    add form

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,9 @@
+$(document).ready(function () {
+    $('#reader__filescan_input').click(function () {
+        $('#dnid').val('')
+    });
+});
+
 function onScanSuccess(decodedText, decodedResult) {
     // handle the scanned code as you like, for example:
     $('#dnid').val(decodedText);
@@ -10,3 +16,5 @@ function onScanFailure(error) {
 
 let html5QrcodeScanner = new Html5QrcodeScanner("reader", { fps: 10, qrbox: { width: 250, height: 250 } },/* verbose= */ false);
 html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+
+//reader__filescan_input
